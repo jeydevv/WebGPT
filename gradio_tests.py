9a9aab82d2e4da84ec407141557dca18c5dd918f
Josh <jas74@sussex.ac.uk>
2023-06-28 18:43:40 +0100

    GUI tweaks

--- gradio_tests.py
+++ gradio_tests.py
@@ -11,6 +11,9 @@ gui = gr.Interface(
     inputs=gr.Textbox(label="Homepage URL", lines=1, placeholder="Homepage URL, e.g. 'brightminded.com'..."),
     outputs=gr.Textbox(label="Output"),
     allow_flagging="manual",
+    flagging_options=[("Save Output", "save")],
+    flagging_dir="saves",
+    css="styles.css",
     title="BrightGPT",
     description="<center>A website analysis and SEO reporting GPT-agent using a GUI and implementing vector storage</center>"
 )
